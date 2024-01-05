131239ba16404f94d27b66774fbdade538e0c620
junhea <97426534+junhea@users.noreply.github.com>
2024-01-05 10:55:26 +0900

    feat: state usage demo

--- docs/js/App.js
+++ docs/js/App.js
@@ -1,7 +1,21 @@
 import Component from "./core/Component.js";
 
 export default class App extends Component {
+  setup() {
+    this.state = { enabled: false };
+  }
+
   template() {
-    return `<h1>Hello world</h1>`;
+    return `<h1 style="transition: color 1s; color: ${
+      this.state.enabled ? "red" : "black"
+    }">Hello world</h1>`;
+  }
+
+  mounted() {
+    const title = this.target.querySelector("h1");
+    title.onclick = () => {
+      console.log(this.state.enabled);
+      this.setState({ enabled: !this.state.enabled });
+    };
   }
 }
