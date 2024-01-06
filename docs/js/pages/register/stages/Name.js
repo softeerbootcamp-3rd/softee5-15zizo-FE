5a74f2c59436ad799580407d01d7acfbcff465a3
chjih <81521128+chjih@users.noreply.github.com>
2024-01-06 22:51:30 +0900

    feat: 이름 입력 화면 구현

---
+++ docs/js/pages/register/stages/Name.js
@@ -0,0 +1,95 @@
+import Component from "../../../core/Component.js";
+
+export default class NameRegister extends Component{
+    setup(){
+        this.state ={
+            name:"",
+            modal:false
+        };
+    }
+
+    template(){
+        return `
+            <div class="stage-container">
+                <div class="name-container">
+                    <div class="question-container" style="text-align: center;">
+                        <p class="question">이름이 무엇인가요?</p>
+                        <div>
+                        <p class="description">저장 후엔 변경할 수 없습니다.</p>
+                        <p class="bold">프로필에 표시되는 이름입니다.</p>
+                        </div>
+                    </div>
+                    <div class="input-name-container">
+                        <input data-component="name-input" class="input-name" type="search" name="name" 
+                        placeholder="이름을 입력해주세요 (3~5자)" autofocus/>
+                    </div>
+                </div>
+                <div class="next-button-container">
+                    <button data-component="next-btn" type="button" class="carting-button ${this.state.name.length>0?"clicked":"disabled"}" data-component="next-btn">다음</button>
+                </div>
+            </div>
+            ${
+                this.state.modal?
+                `
+                <div data-component="modal-back" class="modal-container">
+                    <div class="modal modal-center">
+                        <div class="hi-container">
+                            <div class="hi-content">
+                                <img src="./img/waving_hand.png" style="width: 44px; height: 44px;">
+                                <p class="hi">${this.state.name}님, 반가워요!</p>
+                            </div>
+                            <p class="hi-description">카팅을 즐기기 위해선<br/>내 정보를 입력해야해요!</p>
+                        </div>
+                        <div class="hi-button-container">
+                            <button data-component="start-btn" class="hi-button color" type="button">지금 시작하기</button>
+                            <button data-component="edit-btn" class="hi-button" type="button">이름 수정하기</button>
+                        </div>
+                    </div>
+                </div>
+                `:""
+            }
+        `
+    }
+
+    mounted(){
+        const input = document.querySelector('[data-component="name-input"]');
+        const nextBtn = document.querySelector('[data-component="next-btn"]');
+
+        input.oninput = (e) =>{
+            if(e.target.value.length!=0){
+                this.state.name = e.target.value;
+                nextBtn.classList.remove("disabled");
+                nextBtn.classList.add("clicked");
+            }else{
+                nextBtn.classList.remove("clicked");
+                nextBtn.classList.add("disabled");
+            }
+        };
+
+        input.onkeypress = (e) => {
+            if(e.keyCode===13){
+                nextBtn.click();
+            }
+        }
+
+        nextBtn.onclick = () =>{
+            if(this.state.name.length!=0){
+                console.log("modal");
+                this.setState({modal:true});
+            }
+        }
+
+        const startBtn = document.querySelector('[data-component="start-btn"]');
+        if(startBtn){
+            startBtn.onclick = () =>{
+                console.log("start");
+            }
+    
+            const editBtn = document.querySelector('[data-component="edit-btn"]');
+            editBtn.onclick = () =>{
+                this.setState({modal: false});
+                input.focus();
+            }
+        }
+    }
+}
