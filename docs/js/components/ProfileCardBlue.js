0d4f9f82d8d13ffea02ce3400112a21333c8d811
junhea <97426534+junhea@users.noreply.github.com>
2024-01-09 09:46:27 +0900

    feat: add prop for on click events

--- docs/js/components/ProfileCardBlue.js
+++ docs/js/components/ProfileCardBlue.js
@@ -2,7 +2,9 @@ import { ProfileCard } from "./ProfileCard.js";
 
 export function ProfileCardBlue(props) {
   return `
-    <div class="profile-container-backround">
+    <div class="profile-container-backround" data-component="open-modal" data-order-id="${
+      props.id
+    }">
         <img class="profile-arrow" src="./img/arrow.svg" />
         ${ProfileCard({ ...props, color: "blue" })}
         </div>
