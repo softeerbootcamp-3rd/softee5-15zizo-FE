b14fbdab7004a3df93dd21a78fac04acaa157dbf
junhea <97426534+junhea@users.noreply.github.com>
2024-01-09 12:22:52 +0900

    feat: api implementation & wrap http requests

--- docs/js/utils/api.js
+++ docs/js/utils/api.js
@@ -1,20 +1,33 @@
-import { httpGet } from "./http.js";
-const API_BASE_URL = "http://api.base/api";
+import { httpDelete, httpGet, httpPatch, httpPost } from "./http.js";
+const API_BASE_URL = "https://8c6e-221-149-4-114.ngrok-free.app/api/v1";
 
 export async function getOrders() {
-  // return (await httpGet(`${API_BASE_URL}/v1/orders`)).json();
-  const orders = [];
-
-  // type: 0->비활성화 1->차 2->뚜벅이
-  for (let i = 0; i < 10; i++) {
-    orders.push({
-      x: 33.450701 + Math.random() * 0.01,
-      y: 126.570667 + Math.random() * 0.01,
-      id: i,
-      type: i % 3,
-    });
-  }
-  return orders;
+  return (await httpGet(`${API_BASE_URL}/members`))?.json();
 }
 
-export async function postProfile(data) {}
+export async function postProfile(data) {
+  return (
+    await httpPost(`${API_BASE_URL}/members`, {
+      nickname: data.nickName,
+      gender: data.gender,
+      info: data.info,
+      hasCompany: data.hasCompany,
+      companyInfo: data.companyInfo,
+      hasCar: data.hasCar,
+      age: data.age,
+      location: {
+        lat: data.location.lat,
+        lng: data.location.lng,
+      },
+    })
+  )?.json();
+}
+
+export async function logout() {
+  return await httpDelete(`${API_BASE_URL}/members`);
+}
+
+// actions:  "REQUEST" | "ACCEPT" | "REJECT" | "MEET" | "RESTART"
+export async function requestCarting(action, partnerId) {
+  return await httpPatch(`${API_BASE_URL}/members`, { action, partnerId });
+}
